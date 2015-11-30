adadc9b62d408925549f20b39620a8a9beb055c1
Pranay Shashank Adavi <shashank.ap28@gmail.com>
2015-11-29 23:26:10 -0500

    commit final merged dataset

--- Name Analysis/src/Merge.java
+++ Name Analysis/src/Merge.java
@@ -11,10 +11,11 @@ public class Merge {
 	public static void main(String[] args) throws IOException {
 
 		final String NEW_LINE_SEPARATOR = "\n";
-		final String FILE_HEADER = "fullname,gender,country";
+		final String FILE_HEADER = "Fullname,Gender,Country";
 		ArrayList<Person> person = new ArrayList<Person>();
-		for(int i=1;i<=42;i++){
-			File csvFile = new File("Ireland"+i+".csv");
+		for(int i=0;i<3;i++){
+			String[] country={"Netherlands-Merge","Ireland-Merge","Poland-Merge"};
+			File csvFile = new File(country[i]+".csv");
 			BufferedReader br = new BufferedReader(new FileReader(csvFile));
 			String line = null;
 
@@ -24,25 +25,31 @@ public class Merge {
 				while ((line = br.readLine()) != null) {
 					count++;
 					// use comma as separator
+					
 					String[] cols = line.split(",");
+					if(cols[0].equals("fullname")&&cols[1].equals("gender")&&cols[2].equals("country")) continue;
+					person.add(new Person(cols[0],cols[1],cols[2]));
+					
+					//For merging the final csv's
 
-					if(cols[6].isEmpty()||cols[6]==null){
+					/*For merging individual contry csv's
+					 * if(cols[6].isEmpty()||cols[6]==null){
 						person.add(new Person(cols[1], cols[2], "Ireland"));
 					}
 					else{
 						person.add(new Person(cols[1], cols[2], cols[6]));
-					}
+					//}
 					if (cols[17].isEmpty() || cols[17] == null || cols[18].isEmpty() || cols[18] == null) continue;
 					person.add(new Person(cols[17], "M", cols[6]));
-					person.add(new Person(cols[18], "F", cols[6]));
+					person.add(new Person(cols[18], "F", cols[6]));*/
 				}
 			}  catch (Exception e) {
-				System.out.println("Wohoo " + count);
+				//System.out.println("Wohoo " + count);
 			}
 			FileWriter fileWriter = null;
 
 			try {
-				fileWriter = new FileWriter("Ireland-Merge.csv");
+				fileWriter = new FileWriter("Name-Merge.csv");
 				//Write the CSV file header
 				fileWriter.append(FILE_HEADER.toString());
 				//Add a new line separator after the header
